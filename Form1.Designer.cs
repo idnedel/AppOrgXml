0db4361cd90f7309b00b410c7b2dc27b255200d9
Matheus Nedel <nedel.matheus@gmail.com>
2025-09-02 14:24:45 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace AppOrgXml
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+#region Código gerado pelo Windows Form Designer
+
+/// <summary>
+/// Método necessário para suporte ao Designer - não modifique 
+/// o conteúdo deste método com o editor de código.
+/// </summary>
+private void InitializeComponent()
+        {
+            this.btn_iniciar = new System.Windows.Forms.Button();
+            this.btn_parar = new System.Windows.Forms.Button();
+            this.btn_reiniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_iniciar
+            // 
+            this.btn_iniciar.Location = new System.Drawing.Point(12, 31);
+            this.btn_iniciar.Name = "btn_iniciar";
+            this.btn_iniciar.Size = new System.Drawing.Size(126, 25);
+            this.btn_iniciar.TabIndex = 0;
+            this.btn_iniciar.Text = "Iniciar";
+            this.btn_iniciar.UseVisualStyleBackColor = true;
+            // 
+            // btn_parar
+            // 
+            this.btn_parar.Location = new System.Drawing.Point(12, 62);
+            this.btn_parar.Name = "btn_parar";
+            this.btn_parar.Size = new System.Drawing.Size(126, 25);
+            this.btn_parar.TabIndex = 1;
+            this.btn_parar.Text = "Parar";
+            this.btn_parar.UseVisualStyleBackColor = true;
+            this.btn_parar.Click += new System.EventHandler(this.btn_parar_click);
+            // 
+            // btn_reiniciar
+            // 
+            this.btn_reiniciar.Location = new System.Drawing.Point(12, 93);
+            this.btn_reiniciar.Name = "btn_reiniciar";
+            this.btn_reiniciar.Size = new System.Drawing.Size(126, 25);
+            this.btn_reiniciar.TabIndex = 2;
+            this.btn_reiniciar.Text = "Reiniciar";
+            this.btn_reiniciar.UseVisualStyleBackColor = true;
+            this.btn_reiniciar.Click += new System.EventHandler(this.btn_reiniciar_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ações do serviço OrgXml";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 131);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(64, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status Atual";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(671, 429);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_reiniciar);
+            this.Controls.Add(this.btn_parar);
+            this.Controls.Add(this.btn_iniciar);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_iniciar;
+        private System.Windows.Forms.Button btn_parar;
+        private System.Windows.Forms.Button btn_reiniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+
+    
+
+}
+
+    
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Windows.Forms;
+
+namespace AppOrgXml
+{
+    public partial class Form1 : Form
+    {
+
+        private Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 1000; // 1 segundo
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceController sc = new ServiceController("OrgXmlService");
+                lblStatus.Text = ($"Status do Serviço: {sc.Status}");
+            }
+            catch (Exception)
+            {
+                lblStatus.Text = "Serviço não instalado.";
+            }
+
+        }
+
+
+        private void btn_iniciar_click(object sender, EventArgs e)
+        {       
+            try
+                 {
+                        ServiceController serviceController = new ServiceController("OrgXmlService");
+
+                            if (serviceController.Status == ServiceControllerStatus.Stopped ||
+                                serviceController.Status == ServiceControllerStatus.Paused)
+                            {
+                                serviceController.Start();
+                                serviceController.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(10));
+                                MessageBox.Show("Serviço iniciado com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show("O serviço já está em execução ou não pode ser iniciado.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Erro ao iniciar o serviço: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+        }
+
+        private void btn_parar_click(object sender, EventArgs e)
+        {
+            ServiceController serviceController = new ServiceController("OrgXmlService");
+
+            if (serviceController.Status == ServiceControllerStatus.Running)
+            {
+                serviceController.Stop();
+                serviceController.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(10));
+                MessageBox.Show("Serviço parado com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("O serviço não está em execução ou não pode ser parado.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btn_reiniciar_click(object sender, EventArgs e)
+        {
+            ServiceController serviceController = new ServiceController("OrgXmlService");
+
+            if (serviceController.Status == ServiceControllerStatus.Running)
+            {
+                serviceController.Stop();
+                serviceController.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(10));
+                serviceController.Start();
+                serviceController.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(10));
+                MessageBox.Show("Serviço iniciado com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
